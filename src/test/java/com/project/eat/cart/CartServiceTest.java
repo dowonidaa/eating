6d34adacc110f926a18bd6a3e28a083a87cf9298
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-05-21 16:19:30 +0900

    - GlobalController n+1 문제 수정 조회 최적화

---
+++ src/test/java/com/project/eat/cart/CartServiceTest.java
@@ -0,0 +1,23 @@
+package com.project.eat.cart;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class CartServiceTest {
+
+    @Autowired
+    private CartService cartService;
+
+    @Test
+    void cartItemsCount() {
+    }
+
+}
