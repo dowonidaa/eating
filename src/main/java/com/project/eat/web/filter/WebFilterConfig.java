a8818039e928cb3073076ee5a39bebf258798373
dowonida <150896886+dowonidaa@users.noreply.github.com>
2024-05-28 14:01:08 +0900

    - 접속 로그 필터 적용

---
+++ src/main/java/com/project/eat/web/filter/WebFilterConfig.java
@@ -0,0 +1,20 @@
+package com.project.eat.web.filter;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebFilterConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/","/main");
+
+        return filterFilterRegistrationBean;
+    }
+}
